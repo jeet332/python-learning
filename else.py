2b27b617e2a754b4f5c2b5349cba88ec71ef959b
jeet332 <indrajeet3333333@gmail.com>
2025-04-30 11:59:00 +0530

    Add files via upload

---
+++ else.py
@@ -0,0 +1,27 @@
+# age=12
+# if age>=18:
+#     print("you are an adult")
+# else:
+#     print(" you are not an adult")
+#
+# a = 10
+#
+# if a == 10:
+#     print('a is equal 10')
+# else:
+# #     print('a is not equal 20')
+# temp=20
+# if temp < 30:
+#     print("temp is normal")
+# else:
+#     print("temp is hot")
+
+a = 10
+b = 20
+c = 30
+if a > b and a > c:
+    print("a is greater")
+elif b > a and b > c:
+    print("b is greater")
+else:
+    print ("c is greater")
