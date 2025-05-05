c6d9f1280cbb2fe0029207841367f78cef920fdb
jeet332 <indrajeet3333333@gmail.com>
2025-05-05 13:01:30 +0530

    Add files via upload

---
+++ while loop.py
@@ -0,0 +1,20 @@
+# num=1
+# while(num<=5):
+#     print("num",num)
+#     num +=1
+#
+# A=2
+# while(A<5):
+#     print("A",A)
+#     A -=1
+
+# Multi=2
+# while(Multi<=20):
+#     print("Multi",Multi)
+#     Multi += 2
+
+n=int(input("Enter your number: "))
+num=1
+while (num<=10):
+    print(n,"*",num,"=",n*num)
+    num +=1
